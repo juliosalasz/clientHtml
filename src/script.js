11d3ded2b2bf447da7417f000947fe87a0c5ecb8
Julio Salas <jscomic@gmail.com>
2023-07-20 22:25:48 -0500

    New Design and bootstrap integration

--- src/script.js
+++ src/script.js
@@ -8,23 +8,18 @@ let infoObject = {
 function brandName(value) {
   // api object
   infoObject = { ...infoObject, brand: value };
+  console.log(infoObject);
 }
 
-// radio buttons background
+//Select button
 
-const lineColor = "1px solid #4c7a50";
-
-function typeChoice(id) {
-  const radioChoice = document.getElementsByClassName("radio-choice");
-
-  for (i = 0; i < radioChoice.length; i++) {
-    radioChoice[i].classList.remove("active");
-    if (radioChoice[i].children[0].value === id) {
-      radioChoice[i].classList.add("active");
-      infoObject = { ...infoObject, type: id };
-      document.getElementById("line").style.borderRight = lineColor;
-    }
+function getSelectValue(value) {
+  if (value === "null") {
+    return;
+  } else {
+    infoObject = { ...infoObject, type: value };
   }
+  console.log(infoObject);
 }
 
 //temperature
@@ -36,56 +31,98 @@ function rangeSlide(value, min, max) {
   const backValue = (value - min) / (max - min);
   const color = `rgb(255, 199, 54, ${backValue})`;
   document.getElementById("rangeSlider").style.backgroundColor = color;
+  console.log(infoObject);
 }
 
 //calculate
 
-const resultMessage = document.getElementById("resultMessage");
+const resultMessage = document.getElementById("resultModal");
 let highDensity;
 let lowDensity;
 
 function calculate() {
-  if (infoObject.type === "arroz") {
+  //check for info
+
+  if (!infoObject.brand) {
+    document.getElementById("brand-error").classList.add("active");
+    console.log("No brand");
+  } else if (!infoObject.type) {
+    document.getElementById("type-error").classList.add("active");
+    console.log("No type");
+  } else {
+    document.getElementById("brand-error").classList.remove("active");
+    document.getElementById("type-error").classList.remove("active");
+    console.log("calc ready");
+  }
+
+  //Calculate the number
+
+  if (infoObject.type === "rice") {
     highDensity = 6.4 - 0.35 * infoObject.temperature;
 
     lowDensity = 5.6 - 0.12 * infoObject.temperature;
   }
-  if (infoObject.type === "cafe") {
+  if (infoObject.type === "coffee") {
     highDensity = 4 - 0.35 * infoObject.temperature;
 
     lowDensity = 3.5 - 0.12 * infoObject.temperature;
-    infoObject = { ...infoObject, type: "café" };
   }
 
-  resultMessage.innerHTML = `<div class="result-wrapper">
-  <div class="result-message">
-    <p class="result-intro">
-      EL TIEMPO DE VIDA DE TU ${infoObject.type.toUpperCase()} DE LA MARCA
-      <span class="bold-message">${infoObject.brand.toUpperCase()}</span>
-    </p>
-    <div class="results">
-      <p class="result-text">Es de</p>
-      <div class="result-display">
-      ${
-        highDensity < 0
-          ? `<p class= "result-text">Su producto ya expiro</p>`
-          : `   <h3 class="result-bold">${highDensity.toFixed(2)} Meses</h3>
-      <p class="result-text">en polietileno de alta densidad</p>`
-      }
-     
+  //Insert info in Modal
+
+  resultMessage.innerHTML = `
+  <div class="modal-dialog" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <p class="result-intro p-4">
+        BRAND:
+        <span class="bold-message">${infoObject.brand.toUpperCase()}</span>
+      </p>
+    </div>
+    <div class="modal-body">
+      <div class="result-message p-4">
+        <div class="d-flex flex-column gap-4">
+          <h3 class="result-bold">You product shelf life is:</h3>
+          <p class="result-text">
+          ${
+            highDensity < 0
+              ? `<p class= "result-text">Your product in high density
+              polyethylene has expired.</p>`
+              : `<span class="result-bold">${highDensity.toFixed(
+                  2
+                )} Months</span> in high density
+              polyethylene.`
+          }
+            
+          </p>
+          <p class="result-text">
+          ${
+            lowDensity < 0
+              ? `<p class= "result-text">Your product in low density
+              polyethylene has expired.`
+              : `          <span class="result-bold">${lowDensity.toFixed(
+                  2
+                )} Months</span> in low density
+              polyethylene.`
+          }
+  
+          </p>
+        </div>
       </div>
-      <h3 class="y">Y</h3>
-      <div class="result-display">
-      ${
-        lowDensity < 0
-          ? `<p class= "result-text">Su producto ya expiro</p>.`
-          : `<h3 class="result-bold">${lowDensity.toFixed(2)} Meses</h3>
-          <p class="result-text">en polietileno de baja densidad.</p>`
-      }
-      </div>
+    </div>
+    <div class="modal-footer">
+      <button
+        type="button"
+        class="btn btn-secondary"
+        data-bs-dismiss="modal"
+      >
+        Close
+      </button>
     </div>
   </div>
 </div>`;
-}
 
-console.log("GIT");
+  //open Modal
+  var myModal = new bootstrap.Modal(document.getElementById("resultModal"));
+  myModal.show();
+}
